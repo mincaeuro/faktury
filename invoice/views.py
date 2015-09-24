784eb1dbf9d3545f87490ff86e415e5b3d33ce50
minca <mincaeuro@gmail.com>
2015-09-24 16:24:58 +0200

    views

--- invoice/views.py
+++ invoice/views.py
@@ -2,7 +2,19 @@
 from django.http import HttpResponse
 
 # Create your views here.
-
+from .models import *
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, faktura_id):
+	return HttpResponse("You're looking at Faktura: %s." % faktura_id)
+
+def firma(request, firma_id):
+	return HttpResponse("You're looking at Firma: %s." % firma_id)
+
+def zakaznik(request, zakaznik_id):
+	return HttpResponse("You're looking at Zakaznik: %s." % zakaznik_id)
+
+def produkt(request, produkt_id):
+	return HttpResponse("You're looking at Produkt: %s." % produkt_id)
