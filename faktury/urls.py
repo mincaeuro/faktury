d448d635eddd2ac63bd9f43e8c6b17b5342b7561
minca <mincaeuro@gmail.com>
2015-09-25 17:03:34 +0200

    views update

--- faktury/urls.py
+++ faktury/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin	
 
 urlpatterns = [
+	url(r'^', include('invoice.urls')),
+	url(r'^/detail/(?P<faktura_id>[0-9]+)', include('invoice.urls')),
 	url(r'^faktury/', include('invoice.urls')),
 	url(r'^users/', include('invoice.urls')),
     url(r'^admin/', include(admin.site.urls)),
--- invoice/urls.py
+++ invoice/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	#url(r'^$', views.users, name='users'),
+	url(r'^/detail/(?P<faktura_id>[0-9]+)', views.detail, name='detail')
 ]
