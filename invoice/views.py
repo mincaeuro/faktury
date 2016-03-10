3f04480b9a5ed9e57416125d7ec3d65dcd61d4f1
mincaeuro <mincaeuro@gmail.com>
2016-03-10 16:59:14 +0100

    update_16-03-10-16-59-14

--- invoice/views.py
+++ invoice/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template.context_processors import csrf
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .models import *
@@ -9,10 +13,26 @@ from .models import *
 
 def index(request):
 	Fakturas_list = Faktura.objects.all()
+	Login_list = Login.objects.all()
 	t = loader.get_template('invoice/index.html')
-	c = Context({'Fakturas_list': Fakturas_list,})
+	c = Context({'Fakturas_list': Fakturas_list, 'Login_list': Login_list})
 	return HttpResponse(t.render(c))
 
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=user, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render_to_response('index.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
 def detail(request, faktura_id):
 	faktura = get_object_or_404(Faktura, pk=faktura_id)
 	return render(request, 'invoice/details.html', {'faktura': faktura})
