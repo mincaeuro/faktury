71834983edd9941f8ebce1e0d89c3f28aafb7721
mincaeuro <mincaeuro@gmail.com>
2017-05-23 14:56:26 +0200

    Add files via upload
    adding function to sum costs

--- invoice/models.py
+++ invoice/models.py
@@ -2,104 +2,111 @@ from django.db import models
 import uuid
 from django.core.validators import RegexValidator
 from django.db.models import Sum, F
+
+
 # Create your models here.
 
 
 class Login(models.Model):
-	id = models.AutoField(primary_key=True)
-	db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-	user = models.CharField(blank=False, unique=True, max_length=50)
-	email = models.EmailField(blank=False)
-	password = models.CharField(blank=False, max_length=150)
-	def __str__(self):
-		return self.user
+    id = models.AutoField(primary_key=True)
+    db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    user = models.CharField(blank=False, unique=True, max_length=50)
+    email = models.EmailField(blank=False)
+    password = models.CharField(blank=False, max_length=150)
+
+    def __str__(self):
+        return self.user
 
 
-		
 class Firma(models.Model):
-	id = models.AutoField(primary_key=True)
-	db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-	creator = models.ForeignKey(Login, blank=False)
-	nazov = models.CharField(blank=False, max_length=200)
-	meno = models.CharField(blank=False, max_length=50)
-	priezvisko = models.CharField(blank=False, max_length=50)
-	ico = models.CharField(blank=False, max_length=50)
-	dic = models.CharField(blank=False, max_length=50)
-	banka = models.CharField(blank=False, max_length=100)
-	alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Zadajte cislo vo formate IBAN')
-	cislo_uctu = models.CharField(blank=False, max_length=24, validators=[alphanumeric])
-	banky=(
-		('', 'vyberte'),
-		('SUBASKBX', 'Všeobecná úverová banka, a. s.'),
-		('GIBASKBX', 'Slovenská sporiteľňa, a. s.'),
-		('TATRSKBX', 'Tatra banka, a. s.'),
-		('BREXSKBX', 'MBANK – BRE Bank SA, pobočka zahraničnej banky mBank v SR'),
-		('UNCRSKBX', 'UniCredit Bank Czech Republic and Slovakia, a. s., pobočka zahr. banky'),
-		('CEKOSKBX', 'ČSOB – Československá obchodná banka, a.s.'),
-	)
-	cislo_swift = models.CharField(max_length=8,choices=banky,default='', blank=False)
-	platca_DPH = models.CharField(choices=[("20", "ano"),("0", "nie")], max_length=2)
-	telefon = models.CharField(max_length=50)
-	adresa_ulica = models.CharField(max_length=200)
-	adresa_mesto = models.CharField(max_length=90)
-	adresa_PSC = models.CharField(max_length=20)
-	def __str__(self):
-		return self.nazov
-	
+    id = models.AutoField(primary_key=True)
+    db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    creator = models.ForeignKey(Login, blank=False)
+    nazov = models.CharField(blank=False, max_length=200)
+    meno = models.CharField(blank=False, max_length=50)
+    priezvisko = models.CharField(blank=False, max_length=50)
+    ico = models.CharField(blank=False, max_length=50)
+    dic = models.CharField(blank=False, max_length=50)
+    banka = models.CharField(blank=False, max_length=100)
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Zadajte cislo vo formate IBAN')
+    cislo_uctu = models.CharField(blank=False, max_length=24, validators=[alphanumeric])
+    banky = (
+        ('', 'vyberte'),
+        ('SUBASKBX', 'Všeobecná úverová banka, a. s.'),
+        ('GIBASKBX', 'Slovenská sporiteľňa, a. s.'),
+        ('TATRSKBX', 'Tatra banka, a. s.'),
+        ('BREXSKBX', 'MBANK – BRE Bank SA, pobočka zahraničnej banky mBank v SR'),
+        ('UNCRSKBX', 'UniCredit Bank Czech Republic and Slovakia, a. s., pobočka zahr. banky'),
+        ('CEKOSKBX', 'ČSOB – Československá obchodná banka, a.s.'),
+    )
+    cislo_swift = models.CharField(max_length=8, choices=banky, default='', blank=False)
+    platca_DPH = models.CharField(choices=[("20", "ano"), ("0", "nie")], max_length=2)
+    telefon = models.CharField(max_length=50)
+    adresa_ulica = models.CharField(max_length=200)
+    adresa_mesto = models.CharField(max_length=90)
+    adresa_PSC = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nazov
+
+
 class Zakaznik(models.Model):
-	id = models.AutoField(primary_key=True)
-	db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-	owner = models.ForeignKey(Login, blank=False)
-	creator = models.ForeignKey(Firma, blank=False)
-	nazov = models.CharField(blank=True, max_length=200)
-	meno = models.CharField(blank=True, max_length=50)
-	cislo = models.CharField(blank=True, max_length=50)
-	priezvisko = models.CharField(blank=True, max_length=50)
-	adresa_ulica = models.CharField(max_length=200)
-	adresa_mesto = models.CharField(max_length=90)
-	adresa_psc = models.CharField(max_length=20)
-	ico = models.CharField(blank=False, max_length=50)
-	dic = models.CharField(blank=False, max_length=50)
-	icdph = models.CharField(blank=False, max_length=50)
-	banka = models.CharField(blank=False, max_length=100)
-	cislo_uctu = models.CharField(blank=False, max_length=100)
-	email = models.EmailField(blank=False)
-	telefon = models.CharField(max_length=50)
-	def __str__(self):
-		return self.nazov
-	
+    id = models.AutoField(primary_key=True)
+    db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(Login, blank=False)
+    creator = models.ForeignKey(Firma, blank=False)
+    nazov = models.CharField(blank=True, max_length=200)
+    meno = models.CharField(blank=True, max_length=50)
+    cislo = models.CharField(blank=True, max_length=50)
+    priezvisko = models.CharField(blank=True, max_length=50)
+    adresa_ulica = models.CharField(max_length=200)
+    adresa_mesto = models.CharField(max_length=90)
+    adresa_psc = models.CharField(max_length=20)
+    ico = models.CharField(blank=False, max_length=50)
+    dic = models.CharField(blank=False, max_length=50)
+    icdph = models.CharField(blank=False, max_length=50)
+    banka = models.CharField(blank=False, max_length=100)
+    cislo_uctu = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(blank=False)
+    telefon = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nazov
+
+
 class Faktura(models.Model):
-	id = models.AutoField(primary_key=True)
-	db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-	owner = models.ForeignKey(Login, blank=False)
-	creator = models.ForeignKey(Firma, blank=False)
-	created_for = models.ForeignKey(Zakaznik, blank=False)
-	cislo_faktury = models.CharField(blank=False, max_length=150)
-	datum_vystavenia = models.DateField(blank=False)
-	datum_splatnosti = models.DateField(blank=False)
-	datum_dodania = models.DateField(blank=False)
-	konstantny_symbol = models.CharField(blank=False, max_length=150)
-	doprava = models.CharField(blank=True, max_length=150)
-	komentar = models.TextField(max_length=3000)
-	def __str__(self):
-		return self.cislo_faktury
-	
-	
+    id = models.AutoField(primary_key=True)
+    db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(Login, blank=False)
+    creator = models.ForeignKey(Firma, blank=False)
+    created_for = models.ForeignKey(Zakaznik, blank=False)
+    cislo_faktury = models.CharField(blank=False, max_length=10)
+    datum_vystavenia = models.DateField(blank=False)
+    datum_splatnosti = models.DateField(blank=False)
+    datum_dodania = models.DateField(blank=False)
+    konstantny_symbol = models.CharField(blank=False, max_length=150)
+    doprava = models.CharField(blank=True, max_length=150)
+    komentar = models.TextField(max_length=3000)
+
+    def __str__(self):
+        return self.cislo_faktury
+
+
 class Polozky(models.Model):
-	id = models.AutoField(primary_key=True)
-	db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-	owner = models.ForeignKey(Login, blank=False)
-	faktura = models.ForeignKey(Faktura, blank=False)
-	dan = models.DecimalField(blank=False, max_digits=20, decimal_places=2)
-	nazov = models.CharField(blank=False, max_length=300)
-	mnozstvo = models.IntegerField(blank=False)
-	kod = models.CharField(blank=True, max_length=100)
-	cena = models.DecimalField(blank=False, max_digits=20, decimal_places=2)
-	
-	def spolu(self):
-		return self.mnozstvo * self.cena
-	spocitaj = models.IntegerField(spolu, editable=False)
-	def __str__(self):
-		return self.nazov
+    id = models.AutoField(primary_key=True)
+    db_uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(Login, blank=False)
+    faktura = models.ForeignKey(Faktura, blank=False)
+    dan = models.DecimalField(blank=False, max_digits=20, decimal_places=2)
+    nazov = models.CharField(blank=False, max_length=300)
+    mnozstvo = models.IntegerField(blank=False)
+    kod = models.CharField(blank=True, max_length=100)
+    cena = models.DecimalField(blank=False, max_digits=20, decimal_places=2)
 
+    def spolu(self):
+        return self.mnozstvo * self.cena
 
+    spocitaj = models.IntegerField(default=0, editable=False)
+
+    def __str__(self):
+        return self.nazov
--- invoice/views.py
+++ invoice/views.py
@@ -6,96 +6,104 @@ from django.shortcuts import render_to_response, redirect
 from django.template.context_processors import csrf
 from django.contrib.auth import authenticate, login, logout
 
-
 # Create your views here.
 from .models import *
 from .forms import LoginForm
 
 
 def index(request):
-	Fakturas_list = Faktura.objects.all()
-	Login_list = Login.objects.all()
-	return render(request, 'invoice/index.html', {'Fakturas_list': Fakturas_list, 'Login_list': Login_list, 'form': LoginForm})
+    Fakturas_list = Faktura.objects.all()
+    Login_list = Login.objects.all()
+    return render(request, 'invoice/index.html',
+                  {'Fakturas_list': Fakturas_list, 'Login_list': Login_list, 'form': LoginForm})
 
-	
+
 ####custom login - not in use
 
 def log_in(request):
-	if request.method == 'POST':
-		form = LoginForm(request.POST or None)
-		if form.is_valid():
-			username = User.objects.get(email=form.cleaned_data['email'])
-			password = form.cleaned_data['password']
-			user = authenticate(username=username, password=password)
-			if user:
-				if user.is_active:
-					auth_login(request, user)
-					return render(request, 'invoice/index.html', {'Fakturas_list': Fakturas_list, 'Login_list': Login_list})
-			else:
-				form = LoginForm(request.POST or None)
-			return HttpResponseRedirect('/?there_you_go')
-	else:
-		form = LoginForm(request.POST or None)
-	return HttpResponseRedirect('/?no_login_requested')
+    if request.method == 'POST':
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            username = User.objects.get(email=form.cleaned_data['email'])
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    auth_login(request, user)
+                    return render(request, 'invoice/index.html',
+                                  {'Fakturas_list': Fakturas_list, 'Login_list': Login_list})
+            else:
+                form = LoginForm(request.POST or None)
+            return HttpResponseRedirect('/?there_you_go')
+    else:
+        form = LoginForm(request.POST or None)
+    return HttpResponseRedirect('/?no_login_requested')
 
 
 @login_required(login_url='/logmein/')
 def detail(request, faktura_id):
-	faktura = get_object_or_404(Faktura, pk=faktura_id)
-	polozky = Polozky.objects.filter(faktura=faktura_id)
-	#zakaznik = Zakaznik.objects.filter(faktura=faktura.db_uuid)
-	zakaznik = Zakaznik.objects.get(faktura=faktura.id)
-	t = loader.get_template('invoice/details.html')
-	c = Context({'faktura': faktura, 'polozky': polozky, 'zakaznik': zakaznik})
-	return HttpResponse(t.render(c))
+    faktura = get_object_or_404(Faktura, pk=faktura_id)
+    polozky = Polozky.objects.filter(faktura=faktura_id)
+    # zakaznik = Zakaznik.objects.filter(faktura=faktura.db_uuid)
+    zakaznik = Zakaznik.objects.get(faktura=faktura.id)
+    sucet = Polozky.objects.filter(faktura=faktura_id).aggregate(total=Sum(F('mnozstvo') * F('cena')))['total']
+    t = loader.get_template('invoice/details.html')
+    c = Context({'faktura': faktura, 'polozky': polozky, 'zakaznik': zakaznik, 'sucet': sucet})
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def firmas_all(request):
-	Firmas_list = Firma.objects.all()
-	t = loader.get_template('invoice/firmas.html')
-	c = Context({'Firmas_list': Firmas_list,})
-	return HttpResponse(t.render(c))
+    Firmas_list = Firma.objects.all()
+    t = loader.get_template('invoice/firmas.html')
+    c = Context({'Firmas_list': Firmas_list, })
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def zakaznik_all(request):
-	zakaznik_list = Zakaznik.objects.all()
-	t = loader.get_template('invoice/zakaznici.html')
-	c = Context({'zakaznik_list': zakaznik_list,})
-	return HttpResponse(t.render(c))
+    zakaznik_list = Zakaznik.objects.all()
+    t = loader.get_template('invoice/zakaznici.html')
+    c = Context({'zakaznik_list': zakaznik_list, })
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def faktury_all(request):
-	Fakturas_list = Faktura.objects.all()
-	t = loader.get_template('invoice/faktury.html')
-	c = Context({'Fakturas_list': Fakturas_list,})
-	return HttpResponse(t.render(c))
+    Fakturas_list = Faktura.objects.all()
+    t = loader.get_template('invoice/faktury.html')
+    c = Context({'Fakturas_list': Fakturas_list, })
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def firma(request, firma_id):
-	firma = get_object_or_404(Firma, pk=firma_id)
-	zoznam = Faktura.objects.filter(creator=firma.id)
-	t = loader.get_template('invoice/firma.html')
-	c = Context({'firma': firma, 'zoznam': zoznam})
-	return HttpResponse(t.render(c))
+    firma = get_object_or_404(Firma, pk=firma_id)
+    zoznam = Faktura.objects.filter(creator=firma.id)
+    t = loader.get_template('invoice/firma.html')
+    c = Context({'firma': firma, 'zoznam': zoznam})
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def zakaznik(request, zakaznik_id):
-	zakaznik = get_object_or_404(Zakaznik, pk=zakaznik_id)
-	zoznam = Faktura.objects.filter(created_for=zakaznik.id)
-	t = loader.get_template('invoice/zakaznik.html')
-	c = Context({'zakaznik': zakaznik, 'zoznam': zoznam})
-	return HttpResponse(t.render(c))
+    zakaznik = get_object_or_404(Zakaznik, pk=zakaznik_id)
+    zoznam = Faktura.objects.filter(created_for=zakaznik.id)
+    t = loader.get_template('invoice/zakaznik.html')
+    c = Context({'zakaznik': zakaznik, 'zoznam': zoznam})
+    return HttpResponse(t.render(c))
+
 
 @login_required(login_url='/logmein/')
 def edit_detail(request, faktura_id):
-	faktura = get_object_or_404(Faktura, pk=faktura_id)
-	try:
-		zmeny = faktura.cislo_faktury.get(pk=request.POST['komentar'])
-	except(KeyError, Faktura.DoesNotExist):
-			return render(request, 'invoice/details.html#letsedit', {
-				'faktura': faktura,'error_message': "Chyba, ne-existujuci/neaktualny zaznam",
-				})
-	else:
-				zmeny.edit_details += 1
-				zmeny.save()
-	return HttpResponseRedirect(reverse('faktura:faktura', args=(faktura.id,)))
+    faktura = get_object_or_404(Faktura, pk=faktura_id)
+    try:
+        zmeny = faktura.cislo_faktury.get(pk=request.POST['komentar'])
+    except(KeyError, Faktura.DoesNotExist):
+        return render(request, 'invoice/details.html#letsedit', {
+            'faktura': faktura, 'error_message': "Chyba, ne-existujuci/neaktualny zaznam",
+        })
+    else:
+        zmeny.edit_details += 1
+        zmeny.save()
+    return HttpResponseRedirect(reverse('faktura:faktura', args=(faktura.id,)))
