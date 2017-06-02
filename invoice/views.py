c6e2d0916db34318f4e164049d6e967807a3b06e
mincaeuro <mincaeuro@gmail.com>
2017-06-02 15:24:08 +0200

    Add files via upload
    - added cookie bar

--- invoice/views.py
+++ invoice/views.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
